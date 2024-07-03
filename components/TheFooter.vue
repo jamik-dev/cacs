b3313e1274e58b60fb8ff1fb9f20aa09d8e2f2c0
Jamshid <jamik.oik.group@gmail.com>
2024-07-03 22:06:49 +0500

    issue fixed

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="bg-grey-1">
-    <div class="wrapper flex flex-col sm:flew-row sm:items-center justify-between gap-y-2 py-5 sm:py-10">
+    <div class="wrapper flex flex-col sm:flex-row sm:items-center justify-between gap-y-2 py-5 sm:py-10">
       <ULink to="/" class="font-black text-xs sm:text-sm md:text-base lg:text-lg tracking-tighter uppercase font-roboto"> Central Asia Container Service </ULink>
       <p class="text-sm sm:text-base">© 2018 All rights reserved.</p>
     </div>
